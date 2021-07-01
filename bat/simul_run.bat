bd8913e3be68bc0f6fd05b9d870d81045931680a
JEONGU KIM <visitoroct02@gmail.com>
2021-07-01 17:22:34 +0900

    Revert "no message"
    This reverts commit 47b85fccff9231d3c45a5c0417c64676bd552f37.

--- bat/simul_run.bat
+++ bat/simul_run.bat
@@ -1,3 +1,6 @@
 @Echo simul_run Start
-call "C:\Anaconda3\Scripts\activate.bat" py37_64
-call python "%~dp0\..\simul_run.py" 1 4 n
+call "%HOMEPATH%\Anaconda3\Scripts\activate.bat" py37_64
+call python "%~dp0/../simul_run.py" 1 4 n
+
+
+
